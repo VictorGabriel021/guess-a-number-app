f2179979e7f21f8be5e6f5bf22bfa45555aeec56
VictorGabriel021 <victorgabriel.cipa@gmail.com>
2022-01-27 16:40:27 -0300

    feat: Adding header, card and startGameScreen

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,52 @@
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import Card from "../components/Card";
+import Colors from "../constants/color";
+
+const StartGameScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>The Game Screen!</Text>
+      <Card style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Confirm" onPress={() => {}} color={Colors.primary} />
+          </View>
+        </View>
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+  button: {
+    width: 90,
+  },
+});
+
+export default StartGameScreen;
